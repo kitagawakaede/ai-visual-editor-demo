918d5f8fec8a000d34899d6bc302527a2e35a802
kitagawakaede <kaede10084203@gmail.com>
2026-06-23 02:11:54 +0900

    fix: adjust centering offset for scrollbar width

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         style={{
           width: '440px',
           minHeight: '100vh',
-          marginLeft: 'calc(50vw - 220px)',
+          marginLeft: 'calc(50vw - 228px)',
           backgroundColor: '#fcc800',
           backgroundImage: `url(${spBackground})`,
           backgroundRepeat: 'no-repeat',
